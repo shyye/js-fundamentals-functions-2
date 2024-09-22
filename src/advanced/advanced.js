60b43029493c30d39fe75d1e2f65f6d229093166
shyye <emma.art.dev@gmail.com>
2024-09-22 17:31:23 +0200

    Fix advanced

--- src/advanced/advanced.js
+++ src/advanced/advanced.js
@@ -81,8 +81,51 @@ function parseRequest(req) {
 
   // call the other functions below as needed
 
+  // Check valid request
+  try {
+    // Store data in array
+    const rawData = req.split('\n')
+    rawData.shift()
+    rawData.pop()
+
+    // Method
+    request.method = rawData[0].split(' ')[0].trim()
+
+    // Path and eventual query
+    const fullPath = rawData[0].split(' ')[1].trim()
+
+    // Path
+    if (fullPath.includes('?')) {
+      request.path = fullPath.split('?')[0].trim()
+    } else {
+      request.path = fullPath
+    }
+
+    // Query
+    request.query = extractQuery(fullPath)
+
+    let currentIndex = 1
+
+    // Headers
+    while (rawData[currentIndex] !== '' && rawData.length > currentIndex) {
+      parseHeader(rawData[currentIndex], request.headers)
+      currentIndex++
+    }
+
+    // Body
+    currentIndex++ // Move past empty string
+    if (rawData.length > currentIndex) {
+      request.body = parseBody(rawData[currentIndex])
+    }
+  } catch (error) {
+    // console.log(error)
+    return request
+  }
   return request
 }
+// console.log(parseRequest(rawGETRequest))
+// console.log(parseRequest(rawGETRequestComplex))
+// console.log(parseRequest(rawPOSTRequest))
 
 // 2. Create a function named parseHeader that accepts two parameters:
 // - a string for one header, and an object of current headers that must be augmented with the parsed header
@@ -92,7 +135,15 @@ function parseRequest(req) {
 // eg: parseHeader('Authorization: Bearer your_access_token', { Host: 'www.example.com' })
 //        => { Host: 'www.example.com', Authorization: 'Bearer your_access_token'}
 // eg: parseHeader('', { Host: 'www.example.com' }) => { Host: 'www.example.com' }
-function parseHeader(header, headers) {}
+function parseHeader(header, headers) {
+  if (header !== '') {
+    const keyValue = header.split(':')
+    const key = keyValue[0].trim()
+    const value = keyValue[1].trim()
+
+    headers[key] = value
+  }
+}
 
 // 3. Create a function named parseBody that accepts one parameter:
 // - a string for the body
@@ -100,14 +151,43 @@ function parseHeader(header, headers) {}
 // search for JSON parsing
 // eg: parseBody('{"key1": "value1", "key2": "value2"}') => { key1: 'value1', key2: 'value2' }
 // eg: parseBody('') => null
-function parseBody(body) {}
+function parseBody(body) {
+  if (body === '') {
+    return null
+  }
+  return JSON.parse(body)
+}
 
 // 4. Create a function named extractQuery that accepts one parameter:
 // - a string for the full path
 // It must return the parsed query as a JavaScript object or null if no query ? is present
 // eg: extractQuery('/api/data/123?someValue=example') => { someValue: 'example' }
 // eg: extractQuery('/api/data/123') => null
-function extractQuery(path) {}
+function extractQuery(path) {
+  if (!path.includes('?')) {
+    return null
+  }
+
+  path = path.split('?')[1].trim()
+
+  let queryList = []
+  if (path.includes('&')) {
+    queryList = path.split('&')
+  } else {
+    queryList.push(path)
+  }
+
+  const queryObject = {}
+
+  queryList.forEach((qString) => {
+    const queryKeyValue = qString.split('=')
+    const key = queryKeyValue[0].trim()
+    const value = queryKeyValue[1].trim()
+    queryObject[key] = value
+  })
+
+  return queryObject
+}
 
 module.exports = {
   rawGETRequest,
